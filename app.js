7ce2e82a957811e7f97461a24a6c83b6b52b98ec
Bachar Hafez <expc7c19@gmail.com>
2021-12-09 02:00:03 +0200

    50% done with mobile UI

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 console.log("app.js is running");
 
+// code coming from leafletjs api
+// https://leafletjs.com/examples/quick-start/
+
 var map = L.map("map").setView([51.505, -0.09], 13);
 L.tileLayer(
   "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
